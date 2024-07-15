89aaa44241e1b51c853279a4bfa24a34035c33ab
iballi2017 <iballi201@gmail.com>
2024-07-15 22:15:10 +0100

    ngrx implemention first commit

---
+++ src/app/ngrx-todo/ngrx-todo.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NgrxTodoRoutingModule } from './ngrx-todo-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NgrxTodoRoutingModule
+  ]
+})
+export class NgrxTodoModule { }
